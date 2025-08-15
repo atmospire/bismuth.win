47ec6465936101fd3f180b6a65b802c3eadd1b0a
Antonin <flowythe@gmail.com>
2025-08-16 00:06:38 +0200

    lint + prettier

--- src/theme.ts
+++ src/theme.ts
@@ -3,17 +3,17 @@
 import { createTheme, type MantineColorsTuple } from "@mantine/core";
 
 const colors: MantineColorsTuple = [
-  "#fceaff",
-  "#f0d1ff",
-  "#d78df9",
-  "#cb6df6",
-  "#bb41f3",
-  "#b126f2",
-  "#ad18f2",
-  "#970bd8",
-  "#8705c1",
-  "#7500aa"
-]
+    "#fceaff",
+    "#f0d1ff",
+    "#d78df9",
+    "#cb6df6",
+    "#bb41f3",
+    "#b126f2",
+    "#ad18f2",
+    "#970bd8",
+    "#8705c1",
+    "#7500aa",
+];
 
 export const theme = createTheme({
     /* Put your mantine theme override here */
